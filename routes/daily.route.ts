94eb0cebfeef1259cd7765e28a61962241f54310
skawnkk <skawn11114@gmail.com>
2023-07-25 00:29:36 +0900

    [feat] timetable update api

--- routes/daily.route.ts
+++ routes/daily.route.ts
@@ -2,9 +2,15 @@ import express from 'express';
 import * as dailyController from './../controllers/daily.controller';
 const dailyRouter = express.Router();
 
+//monthly
 dailyRouter.get('/monthly', dailyController.getMonthly);
+//daily
 dailyRouter.get('/daily/:id', dailyController.getDaily);
+//daily_todo
 dailyRouter.post('/daily/:id/todo', dailyController.updateTodo);
 dailyRouter.post('/daily/:id/todo/add', dailyController.createTodo);
 dailyRouter.delete('/daily/:id/todo/delete', dailyController.deleteTodo);
+//daily_timetable
+dailyRouter.post('/daily/:id/schedule', dailyController.updateSchedule);
+
 export default dailyRouter;
